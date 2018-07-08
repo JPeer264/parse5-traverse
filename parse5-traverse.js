6568f92c00af9d4febd7b5aca0036badfc85484e
JPeer264 <jan.oster94@gmail.com>
2018-07-08 16:39:54 +0200

    Initial commit

---
+++ parse5-traverse.js
@@ -0,0 +1,37 @@
+/**
+ * the main logic comes from https://github.com/olov/ast-traverse.
+ * A big thanks to @olov.
+ */
+const traverse = (root, options = {}) => {
+  const { pre, post, skipProperty } = options;
+
+  const visit = (node, parent) => {
+    let res;
+
+    if (!node || typeof node.childNodes.length <= 0) {
+      return;
+    }
+
+    if (pre) {
+      res = pre(node, parent);
+    }
+
+    if (res !== false) {
+      node.childNodes.forEach((child) => {
+        if (skipProperty && skipProperty(node)) {
+          return;
+        }
+
+        visit(child, node);
+      });
+    }
+
+    if (post) {
+      post(node, parent);
+    }
+  };
+
+  visit(root, null);
+};
+
+module.exports = traverse;
